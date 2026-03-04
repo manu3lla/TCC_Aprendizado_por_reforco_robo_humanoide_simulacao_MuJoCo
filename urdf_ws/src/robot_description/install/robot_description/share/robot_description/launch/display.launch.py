0c466619b59129b6a48b7962d8832d2899c98283
Letizia L. Baptistella <letizia.baptistella@gmail.com>
2026-03-03 21:09:32 -0300

    adicionando código para visualização do Atom no Rviz

---
+++ urdf_ws/src/robot_description/install/robot_description/share/robot_description/launch/display.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
+import os
+
+def generate_launch_description():
+
+    urdf_file = os.path.join(
+        os.getenv('HOME'),
+        'urdf/src/robot_description/urdf/robot.urdf'
+    )
+
+    robot_description = ParameterValue(
+        Command(['cat ', urdf_file]),
+        value_type=str
+    )
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            output='screen'
+        )
+    ])
